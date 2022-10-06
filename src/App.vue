3daef12a1169e11133268ddbc02f5ad9dc055e85
tHyt-lab <washers.fezzes-0q@icloud.com>
2022-10-06 09:03:34 +0000

    add auth.

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,67 @@
 <template>
-  <el-container>
-    <el-header>
-      <h2>Amplify Vue Workshop</h2>
-    </el-header>
-    <el-container>
-      <el-aside width="200px">
-        <el-menu>
-          <router-link class="routerLink" to="/chat">
-            <el-menu-item>
-              <el-icon>
-                <ChatDotRound />
-              </el-icon>
-              <span>Chat</span>
-            </el-menu-item>
-          </router-link>
-          <router-link class="routerLink" to="/ai">
-            <el-menu-item>
-              <el-icon>
-                <View />
-              </el-icon>
-              <span>AI</span>
-            </el-menu-item>
-          </router-link>
-          <router-link class="routerLink" to="/analytics">
-            <el-menu-item>
-              <el-icon>
-                <DataAnalysis />
-              </el-icon>
-              <span>Analytics</span>
-            </el-menu-item>
-          </router-link>
-          <router-link class="routerLink" to="/webpush">
-            <el-menu-item>
-              <el-icon>
-                <Notification />
-              </el-icon>
-              <span>Web Push</span>
-            </el-menu-item>
-          </router-link>
-        </el-menu>
-      </el-aside>
-      <el-main>
-        <router-view />
-      </el-main>
-    </el-container>
-  </el-container>
+  <!-- 追加① -->
+  <authenticator>
+    <template v-slot="{ user, signOut }">
+      <el-container>
+        <el-header>
+          <h2>Amplify Vue Workshop</h2>
+        </el-header>
+        <el-container>
+          <el-aside width="200px">
+            <el-menu>
+              <el-sub-menu>
+                <template #title>
+                  <el-icon>
+                    <User />
+                  </el-icon>
+                  <span>{{ user.username }}</span>
+                </template>
+                <el-menu-item @click="signOut">Sign Out</el-menu-item>
+              </el-sub-menu>
+              <router-link class="routerLink" to="/chat">
+                <el-menu-item>
+                  <el-icon>
+                    <ChatDotRound />
+                  </el-icon>
+                  <span>Chat</span>
+                </el-menu-item>
+              </router-link>
+              <router-link class="routerLink" to="/ai">
+                <el-menu-item>
+                  <el-icon>
+                    <View />
+                  </el-icon>
+                  <span>AI</span>
+                </el-menu-item>
+              </router-link>
+              <router-link class="routerLink" to="/analytics">
+                <el-menu-item>
+                  <el-icon>
+                    <DataAnalysis />
+                  </el-icon>
+                  <span>Analytics</span>
+                </el-menu-item>
+              </router-link>
+              <router-link class="routerLink" to="/webpush">
+                <el-menu-item>
+                  <el-icon>
+                    <Notification />
+                  </el-icon>
+                  <span>Web Push</span>
+                </el-menu-item>
+              </router-link>
+            </el-menu>
+          </el-aside>
+          <el-main>
+            <router-view />
+          </el-main>
+        </el-container>
+      </el-container>
+    </template>
+  </authenticator>
 </template>
-
 <script setup lang="ts">
-import { ChatDotRound, View, Notification, DataAnalysis } from '@element-plus/icons-vue';
+import { Authenticator } from '@aws-amplify/ui-vue';
+import { User, ChatDotRound, View, Notification, DataAnalysis } from '@element-plus/icons-vue';
 </script>
 <style src="./assets/style.css"></style>
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,14 @@ import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/theme-chalk/index.css'
 
+import { Amplify } from 'aws-amplify'
+import awsconfig from './aws-exports'
+import AmplifyVue from '@aws-amplify/ui-vue'
+import '@aws-amplify/ui-vue/styles.css'
+Amplify.configure(awsconfig)
+
 const app = createApp(App)
 app.use(ElementPlus)
 app.use(router)
+app.use(AmplifyVue)
 app.mount('#app')
